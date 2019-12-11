ba2c2626d43a6ba2be962d7fbbba68c823a28949
Roberto Banbanaste <rbanbanaste@outlook.com>
2019-12-11 12:36:33 -0500

    finished mvp

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
+// api -> https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY
+
 function App() {
+  const [stateObject, setStateObject] = useState({});
+
+  useEffect(() => {
+    console.log("logs on component mount");
+    fetch(
+      "https://api.nasa.gov/planetary/apod?api_key=eAhnsa3UIzMgyMUd2D1dtw6uF1WuRaq3etKGFf8x"
+    )
+      .then(response => response.json())
+      .then(jsonObject => {
+        console.log(jsonObject);
+        setStateObject({ ...jsonObject });
+      });
+  }, []);
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      <div>
+        <h1>NASA's</h1>
+        <h2>Picture of the Day for {stateObject.date}</h2>
+      </div>
+      <img src={stateObject.hdurl} />
+      <p>{stateObject.explanation}</p>
     </div>
   );
 }
